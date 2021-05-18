5a5e9427754d311972d137c96b8aee1eb371e524
Michał Węglowski-Hodur <michal@michal.dev>
2021-05-18 20:42:38 +0200

    [feature] Enable localisation

--- api/home/views.py
+++ api/home/views.py
@@ -1,18 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.utils.translation import gettext as _
 
 def home_view(request):
     context = {}
     if request.user.is_authenticated:
-        return HttpResponse("Hello, world")
+        return HttpResponse(_("Hello, world"))
     else:
         return render(request, 'home_default_unlogged.html', context)
 
 
 def register(request):
-    return HttpResponse("Register page")
+    return HttpResponse(_("Register page"))
 
 
 def logout(request):
-    return HttpResponse("Logout page")
+    return HttpResponse(_("Logout page"))
